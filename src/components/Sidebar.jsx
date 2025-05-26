8ee3c0151d0f49c43559527ee7fda657a593c183
Joao <joaotrindade430@gmail.com>
2025-05-26 16:19:43 -0300

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,31 @@
-import styles from './Sidebar.module.css'
+import { PencilLine } from "phosphor-react";
 
-function Sidebar(){
-    return(
-        <aside className={styles.sidebar}>
-            <img src="https://imgs.search.brave.com/uXgNRn2HdIJUAy4mMHtgAZ3jLTD2XsWDqvzK5EFJKn8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvZ2FtZXItMTJ1/bDcyY21jcGppZms4/bS5qcGc" alt="guaxinim" />
-            <div className={styles.profile}>
-                <strong>Joao Vitor</strong>
-                <span>web developer</span>
-            </div>
-            <footer>
-                <a href="#">
-                    editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    )
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://imgs.search.brave.com/uXgNRn2HdIJUAy4mMHtgAZ3jLTD2XsWDqvzK5EFJKn8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvZ2FtZXItMTJ1/bDcyY21jcGppZms4/bS5qcGc"
+        alt="guaxinim"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfNjUepcJE63cHgBtiaamPY5bP9-EQ3l0S7w&s"
+        />
+        <strong>Joao Vitor</strong>
+        <span>web developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
